9a2d2376a4bbfd3f1600a3089d4a2c34d438459a
Joey574 <joeysoroka@outlook.com>
2025-01-04 14:58:57 -0800

    howcookedami is basically working :)

--- howcookedami.c
+++ howcookedami.c
@@ -26,9 +26,30 @@ typedef struct {
     size_t shell;
     size_t gdb;
     size_t html;
+    size_t md;
     size_t failed;
 } LOCs;
 
+typedef struct {
+    size_t elements;
+    string* str;
+} stringarr;
+
+static const unsigned char base64_table[128] = {
+    [0 ... 127] = 255,
+
+    ['A'] = 0,  ['B'] = 1,  ['C'] = 2,  ['D'] = 3,  ['E'] = 4,  ['F'] = 5,
+    ['G'] = 6,  ['H'] = 7,  ['I'] = 8,  ['J'] = 9,  ['K'] = 10, ['L'] = 11,
+    ['M'] = 12, ['N'] = 13, ['O'] = 14, ['P'] = 15, ['Q'] = 16, ['R'] = 17,
+    ['S'] = 18, ['T'] = 19, ['U'] = 20, ['V'] = 21, ['W'] = 22, ['X'] = 23,
+    ['Y'] = 24, ['Z'] = 25, ['a'] = 26, ['b'] = 27, ['c'] = 28, ['d'] = 29,
+    ['e'] = 30, ['f'] = 31, ['g'] = 32, ['h'] = 33, ['i'] = 34, ['j'] = 35,
+    ['k'] = 36, ['l'] = 37, ['m'] = 38, ['n'] = 39, ['o'] = 40, ['p'] = 41,
+    ['q'] = 42, ['r'] = 43, ['s'] = 44, ['t'] = 45, ['u'] = 46, ['v'] = 47,
+    ['w'] = 48, ['x'] = 49, ['y'] = 50, ['z'] = 51, ['0'] = 52, ['1'] = 53,
+    ['2'] = 54, ['3'] = 55, ['4'] = 56, ['5'] = 57, ['6'] = 58, ['7'] = 59,
+    ['8'] = 60, ['9'] = 61, ['+'] = 62, ['/'] = 63
+};
 
 void append(string* str, const char* data) {
     str->str = realloc(str->str, str->len + strlen(data) + 1);
@@ -53,8 +74,13 @@ void clearstr(string* str) {
     str->str = (char*)realloc(str->str, 1);
     str->str[0] = '\x00';
 }
+void printstrarr(stringarr* arr) {
+    for(size_t i = 0; i < arr->elements; i++) {
+        printf("%s\n", arr->str[i].str);
+    }
+}
 
-string parse_repos(char* user, char* pat) {
+stringarr parse_repos(char* user, char* pat, stringarr exclude) {
     const char* CURLCMD = "curl -s -H \"Authorization: token ";
     const char* SUBSTRING = "\"url\": \"";
     const char* URL = "https://api.github.com/user/repos";
@@ -63,13 +89,13 @@ string parse_repos(char* user, char* pat) {
     char recvbuf[512];
     FILE* pipe;
 
-    string finalcmd = { 0, (char*)malloc(1) };
+    string finalcmd = { 0, (char*)malloc(0) };
     append(&finalcmd, CURLCMD);
     append(&finalcmd, pat);
     append(&finalcmd, "\" ");
     append(&finalcmd, URL);
 
-    string html = { 0, (char*)malloc(1) };
+    string html = { 0, (char*)malloc(0) };
 
     pipe = popen(finalcmd.str, "r");
 
@@ -81,11 +107,11 @@ string parse_repos(char* user, char* pat) {
     pclose(pipe);
     free(finalcmd.str);
 
-    string home_repo = { 0, (char*)malloc(1) };
+    string home_repo = { 0, (char*)malloc(0) };
     append(&home_repo, HOMEURL);
     append(&home_repo, user);
 
-    string repos = { 0, (char*)malloc(1) };
+    stringarr repos = { 0, (string*)malloc(1 * sizeof(string)) };
 
     char* searchstring = html.str;
     while ((searchstring = strstr(searchstring, SUBSTRING)) != NULL) {
@@ -94,17 +120,34 @@ string parse_repos(char* user, char* pat) {
         size_t idx = searchstring - html.str;
         size_t eidx = strstr(searchstring, "\"") - html.str;
 
-        if ((strstr(searchstring, "licenses") - html.str > eidx || 
-            strstr(searchstring, "licenses") == NULL) && 
-            eidx - idx > strlen(home_repo.str)) {
+        string tmp = { 0, (char*)malloc(0) };
+        appendn(&tmp, searchstring, eidx - idx);
 
-                appendn(&repos, searchstring, eidx - idx);
-                appendch(&repos, '\n');
+        if (strstr(tmp.str, "licenses") == NULL && tmp.len > strlen(home_repo.str)) {
+            
+            __uint8_t valid = 1;
+            for (size_t i = 0; i < exclude.elements && valid; i++) {
+                if (strstr(tmp.str, exclude.str[i].str) != NULL) {
+                    valid = 0;
+                }
             }
+
+            if (valid) {
+                repos.elements++;
+
+                repos.str = realloc(repos.str, repos.elements * sizeof(string));
+                repos.str[repos.elements - 1].len = 0;
+                repos.str[repos.elements - 1].str = (char*)malloc(0);
+                append(&repos.str[repos.elements - 1], tmp.str);
+                append(&repos.str[repos.elements - 1], "/contents");
+            }
+        }
+
+        free(tmp.str);
     }
 
     #if LOG
-    printf("Repos:\n%s\n", repos.str);
+    printstrarr(&repos);
     #endif
 
     return repos;
@@ -124,57 +167,69 @@ void parse_file(LOCs* locs, string url, const char* pat, const char* fileext) {
     append(&finalcmd, url.str);
     append(&finalcmd, "\"");
 
-    __uint8_t mod = 0;
+    string html = { 0, (char*)malloc(0) };
 
     pipe = popen(finalcmd.str, "r");
     while(fgets(recvbuf, sizeof(recvbuf), pipe) != NULL) {
-
-        char* t = strstr(recvbuf, " loc)");
-
-        if (t != NULL) {
-            size_t sidx = t - recvbuf;
-
-            while(recvbuf[sidx] != '(') { sidx--; }
-            size_t eidx = strstr(&recvbuf[sidx], " ") - recvbuf;
-
-            string lines = { 0, (char*)malloc(1) };
-            appendn(&lines, &recvbuf[sidx + 1], eidx - sidx);
-
-            size_t tmp = atoi(lines.str);
-
-            if (strcmp(fileext, "c") == 0) { locs->c += tmp; }
-            else if (strcmp(fileext, "h") == 0) { locs->h += tmp; }
-            else if (strcmp(fileext, "cpp") == 0) { locs->cpp += tmp; }
-            else if (strcmp(fileext, "hpp") == 0) { locs->hpp += tmp; }
-            else if (strcmp(fileext, "cs") == 0) { locs->csharp += tmp; }
-            else if (strcmp(fileext, "js") == 0) { locs->js += tmp; }
-            else if (strcmp(fileext, "py") == 0) { locs->py += tmp; }
-            else if (strcmp(fileext, "asm") == 0) { locs->as += tmp; }
-            else if (strcmp(fileext, "java") == 0) { locs->java += tmp; }
-            else if (strcmp(fileext, "go") == 0) { locs->go += tmp; }
-            else if (strcmp(fileext, "compute") == 0) { locs->hlsl += tmp; }
-            else if (strcmp(fileext, "cu") == 0) { locs->cuda += tmp; }
-            else if (strcmp(fileext, "cuh") == 0) { locs->cudah += tmp; }
-            else if (strcmp(fileext, "sh") == 0) { locs->shell += tmp; }
-            else if (strcmp(fileext, "gdb") == 0) { locs->gdb += tmp; }
-            else if (strcmp(fileext, "html") == 0) { locs->html += tmp; }
-            else { printf("ERROR PARSING EXT\n"); }
-            mod = 1;
-
-            break;
-        }
-    }
-
-    if (!mod)  {
-        locs->failed++;
+        append(&html, recvbuf);
+        memset(recvbuf, 0, 512);
     }
 
     free(finalcmd.str);
     pclose(pipe);
+
+    size_t tmp = 0;
+
+    size_t sidx = strstr(html.str, "\"content\": \"") - html.str + strlen("\"content\": \"");
+    size_t eidx = strstr(&html.str[sidx], "\"") - html.str;
+    size_t len = eidx - sidx;
+
+    char* searchstring = &html.str[sidx];
+    for(size_t i = 0, j = 0; i < len;) {
+        if(searchstring[i] == '\\' && searchstring[i + 1] == 'n') {
+            i += 2;
+        } else {
+            int val = base64_table[searchstring[i++]] << 18;
+            val |= base64_table[searchstring[i++]] << 12;
+            val |= base64_table[searchstring[i++]] << 6;
+            val |= base64_table[searchstring[i++]];
+
+            if ((char)(val >> 16) == '\n') { tmp++; }
+            if ((char)(val >> 8) == '\n') { tmp++; }
+            if ((char)(val & 0xff) == '\n') { tmp++; }
+        }
+    }
+
+    if (strcmp(fileext, "c") == 0) { locs->c += tmp; }
+    else if (strcmp(fileext, "h") == 0) { locs->h += tmp; }
+    else if (strcmp(fileext, "cpp") == 0) { locs->cpp += tmp; }
+    else if (strcmp(fileext, "hpp") == 0) { locs->hpp += tmp; }
+    else if (strcmp(fileext, "cs") == 0) { locs->csharp += tmp; }
+    else if (strcmp(fileext, "js") == 0) { locs->js += tmp; }
+    else if (strcmp(fileext, "py") == 0) { locs->py += tmp; }
+    else if (strcmp(fileext, "asm") == 0) { locs->as += tmp; }
+    else if (strcmp(fileext, "java") == 0) { locs->java += tmp; }
+    else if (strcmp(fileext, "go") == 0) { locs->go += tmp; }
+    else if (strcmp(fileext, "compute") == 0) { locs->hlsl += tmp; }
+    else if (strcmp(fileext, "cu") == 0) { locs->cuda += tmp; }
+    else if (strcmp(fileext, "cuh") == 0) { locs->cudah += tmp; }
+    else if (strcmp(fileext, "sh") == 0) { locs->shell += tmp; }
+    else if (strcmp(fileext, "gdb") == 0) { locs->gdb += tmp; }
+    else if (strcmp(fileext, "html") == 0) { locs->html += tmp; }
+    else if (strcmp(fileext, "md") == 0) { locs->md += tmp; }
+    else { printf("ERROR PARSING EXT\n"); }
+
+    if (strstr(html.str, "\"content\": \"") == NULL)  {
+        locs->failed++;
+    }
+
+    free(html.str);
 }
 void parse_dir(LOCs* locs, string url, const char* pat) {
     const char* CURLCMD = "curl -s -L -H \"Authorization: token ";
-    const char* SUBSTRING = "\"name\": \"";
+    const char* NAMESUBSTRING = "\"name\": \"";
+    const char* URLSUBSTRING = "\"url\": \"";
+    const char* TYPESUBSTRING = "\"type\": \"";
 
     char recvbuf[512];
     FILE* pipe;
@@ -186,7 +241,7 @@ void parse_dir(LOCs* locs, string url, const char* pat) {
     append(&finalcmd, url.str);
     append(&finalcmd, "\"");
 
-    string html = { 0, (char*)malloc(1) };
+    string html = { 0, (char*)malloc(0) };
 
     pipe = popen(finalcmd.str, "r");
 
@@ -199,24 +254,24 @@ void parse_dir(LOCs* locs, string url, const char* pat) {
     pclose(pipe);
 
 
-    string name = { 0, (char*)malloc(1) };
+    string name = { 0, (char*)malloc(0) };
 
     char* searchstring = html.str;
-    while ((searchstring = strstr(searchstring, SUBSTRING)) != NULL) {
-        searchstring += strlen(SUBSTRING);
+    while ((searchstring = strstr(searchstring, NAMESUBSTRING)) != NULL) {
+        searchstring += strlen(NAMESUBSTRING);
 
         size_t idx = searchstring - html.str;
         size_t eidx = strstr(searchstring, "\"") - html.str;
 
         appendn(&name, searchstring, eidx - idx);
 
-        size_t urlidx = strstr(searchstring, "\"url\": \"") - searchstring + strlen("\"url\": \"");
+        size_t urlidx = strstr(searchstring, URLSUBSTRING) - searchstring + strlen(URLSUBSTRING);
         size_t eurlidx = strstr(&searchstring[urlidx], "\"") - searchstring;
 
         string tmp_url = { 0, (char*)malloc(1) };
         appendn(&tmp_url, &searchstring[urlidx], eurlidx - urlidx);
 
-        size_t typeidx = strstr(searchstring, "\"type\": \"") - searchstring + strlen("\"type\": \"");
+        size_t typeidx = strstr(searchstring, TYPESUBSTRING) - searchstring + strlen(TYPESUBSTRING);
 
         if (searchstring[typeidx] == 'f') {
             
@@ -245,22 +300,14 @@ void parse_dir(LOCs* locs, string url, const char* pat) {
                 strcmp(fileext, "cuh") == 0 ||
                 strcmp(fileext, "sh") == 0 ||
                 strcmp(fileext, "gdb") == 0 ||
-                strcmp(fileext, "html") == 0
+                strcmp(fileext, "html") == 0 ||
+                strcmp(fileext, "md") == 0
             )) {
                 #if LOG
                 printf("File: %s\n", name.str);
                 #endif
 
-                string html_url = { 0, (char*)malloc(1) };
-
-                size_t shtmlidx = strstr(&searchstring[typeidx], "\"html\": \"") - searchstring + strlen("\"html\": \"");
-                size_t ehtmlidx = strstr(&searchstring[shtmlidx], "\"") - searchstring;
-
-                appendn(&html_url, &searchstring[shtmlidx], ehtmlidx - shtmlidx);
-
-                parse_file(locs, html_url, pat, fileext);
-
-                free(html_url.str);
+                parse_file(locs, tmp_url, pat, fileext);
             }
         } else if (searchstring[typeidx] == 'd') {
 
@@ -292,28 +339,51 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    char* user = argv[1];
-    char* pat = argv[2];
+    char* user;
+    char* pat;
+    
+    stringarr excluded = { 0, (string*)malloc(0) };
 
-    string repos = parse_repos(user, pat);
+    for(int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-u") == 0) {
+            if (argc > i + 1) {
+                user = argv[i + 1];
+            }
+        }
+
+        if (strcmp(argv[i], "-t") == 0) {
+            if (argc > i + 1) {
+                pat = argv[i + 1];
+            }
+        }
+        
+        if (strcmp(argv[i], "-e") == 0) {
+            if (argc > i + 1) {
+                excluded.elements++;
+                excluded.str = (string*)realloc(excluded.str, excluded.elements * sizeof(string));
+
+                excluded.str[excluded.elements - 1].len = strlen(argv[i + 1]);
+                excluded.str[excluded.elements - 1].str = argv[i + 1];
+            }
+            
+        }
+    }
+
+    stringarr repos = parse_repos(user, pat, excluded);
+
+    #if LOG
+    printf("Repos:\n");
+    printstrarr(&repos);
+    printf("\n");
+    #endif
 
     LOCs locs;
     memset(&locs, 0, sizeof(locs));
 
-    string tmp = { 0, (char*)malloc(1) };
-    for(size_t i = 0; i < repos.len; i++) {
-        if (repos.str[i] != '\n') {
-            appendch(&tmp, repos.str[i]);
-        } else {
-            append(&tmp, "/contents");
-            printf("Searching: %s\n", tmp.str);
-            parse_dir(&locs, tmp, pat);
-
-            clearstr(&tmp);
-        }
+    for(size_t i = 0; i < repos.elements; i++) {
+        printf("Searching: %s\n", repos.str[i].str);
+        parse_dir(&locs, repos.str[i], pat);
     }
-
-    free(repos.str);
 
     printf("\nLines of Code:"
     "\nC: %lu"
@@ -331,9 +401,10 @@ int main(int argc, char* argv[]) {
     "\nShell: %lu"
     "\nGdb Script: %lu"
     "\nHtml: %lu"
+    "\nMarkdown: %lu"
     "\nFailed to read: %lu files\n",
     locs.c, locs.cpp, locs.h + locs.hpp, locs.csharp, locs.js,
     locs.py, locs.as, locs.java, locs.go, locs.hlsl, locs.cuda,
-    locs.cudah, locs.shell, locs.gdb, locs.html, locs.failed
+    locs.cudah, locs.shell, locs.gdb, locs.html, locs.md, locs.failed
     );
 }
