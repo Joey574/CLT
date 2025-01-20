6dd922116dd51f2b929400975103997ac4df481e
Joey574 <joeysoroka@outlook.com>
2025-01-20 10:51:38 -0800

    trying to rewrite to non blocking for port scanning, looking into doing recon through tor as well, + syn-scan in the future once I can wrap my head around all that header bs

--- Cpp src/disnmap.cpp
+++ Cpp src/disnmap.cpp
@@ -4,18 +4,35 @@
 #include <unistd.h>
 #include <cstring>
 #include <fcntl.h>
+#include <vector>
+
+#define DEBUG 1
+
+inline void success(const std::string&, int);
+inline std::string as_ip(int, int, int, int);
+
+void scan_port_tcp(const std::string& ip, int port, std::vector<int>& fd, std::vector<int>& p);
+void scan_ip(const std::string& ip, void (*scan_type) (const std::string&, int, std::vector<int>&, std::vector<int>&));
 
 
-bool scan_port_tcp(const std::string& ip, int port) {
-    struct sockaddr_in server_addr;
+inline void success(const std::string& ip, int port) {
+    std::cout << "Port " << port << " is open on " << ip << "\n";
+}
+inline std::string as_ip(int a, int b, int c, int d) {
+    return std::to_string(a).append(".").append(std::to_string(b)).append(".").append(std::to_string(c)).append(".").append(std::to_string(d));
+}
 
+
+void scan_port_tcp(const std::string& ip, int port, std::vector<int>& fd, std::vector<int>& p) {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sockfd < 0) {
-        exit(1);
+        std::cerr << "Error creating socket\n";
+        return;
     }
 
     // set up sockaddr
+    struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = inet_addr(ip.data());
@@ -25,43 +42,72 @@ bool scan_port_tcp(const std::string& ip, int port) {
     int result = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
     if (result == 0) {
+        // success
+        success(ip, port);
         close(sockfd);
-        return true;
+    } else if (result == -1 && errno != EINPROGRESS) {
+        // failure
+        close(sockfd);
+    } else if (result == -1 && errno == EINPROGRESS) {
+        // in progress
+        fd.push_back(sockfd);
+        p.push_back(port);
+    }
+}
+
+void scan_ip(const std::string& ip, void (*scan_type) (const std::string&, int, std::vector<int>&, std::vector<int>&)) {
+    std::vector<int> fd;
+    std::vector<int> port;
+
+    for(size_t i = 0; i < 1500; i++) {
+        scan_type(ip, i, fd, port);
     }
 
-    // Use select() to set a timeout
     fd_set write_fds;
     FD_ZERO(&write_fds);
-    FD_SET(sockfd, &write_fds);
+    int max_fd = 0;
 
-    struct timeval timeout = { 1, 0 };
-
-    result = select(sockfd + 1, NULL, &write_fds, NULL, &timeout);
-
-    if (result > 0) {
-        int so_error;
-        socklen_t len = sizeof(so_error);
-        if (getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &so_error, &len) == 0 && so_error == 0) {
-            close(sockfd);
-            return true;
-        }
+    for (size_t i = 0; i < fd.size(); i++) {
+        FD_SET(fd[i], &write_fds);
+        max_fd = fd[i] > max_fd ? fd[i] : max_fd;
     }
 
-    close(sockfd);
-    return false;
-}
+    // Set timeout: { seconds, microseconds }
+    struct timeval timeout = {1, 0};
+    select(max_fd + 1, NULL, &write_fds, NULL, &timeout);
 
-void scan_ip(const std::string& ip, bool scan_type (const std::string&, int)) {
-    for(size_t i = 0; i < 1500; i++) {
-        if (scan_type(ip, i)) {
-            std::cout << "Port " << i << " is open on " << ip << "\n";
+    for (size_t i = 0; i < fd.size(); i++) {
+        if (FD_ISSET(fd[i], &write_fds)) {
+            int so_error;
+            socklen_t len = sizeof(so_error);
+
+            if (getsockopt(fd[i], SOL_SOCKET, SO_ERROR, &so_error, &len) == 0 && so_error == 0) {
+                success(ip, port[i]);
+            }
+            close(fd[i]);
+        } else {
+            close(fd[i]);
         }
     }
+    std::cout << "out\n";
 }
+
 
 int main(int argc, char* argv[]) {
 
-    std::string ip = argv[1];
+    int a = 192;
+    int b = 168;
+    int c = 4;
 
-    scan_ip(ip, &scan_port_tcp);
+    //#pragma omp parallel for
+    for (int d = 0; d < 256; d++) {
+        std::string ip = as_ip(a, b, c, d);
+
+        #if DEBUG
+        std::string o = "Scanning " + ip + "\n";
+        std::cout << o;
+        #endif
+
+        scan_ip(ip, &scan_port_tcp);
+    }
 }
